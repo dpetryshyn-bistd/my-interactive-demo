d97260a06316093e8a4dc066714d4dfa9469d012
dpetryshyn-bistd <d.petryshyn@e-u.edu.ua>
2025-10-20 08:53:48 +0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
-let targetX =100;
-let targetY =100;
-let targetRadius = 150;
+let targetX = 100;
+let targetY = 100;
+let targetRadius = 120;
 let score = 0;
 let timeLeft = 30;
 let gameRunning = false;
 let timer;
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(800, 600);
   noStroke();
   textAlign(CENTER, CENTER);
   textSize(24);
@@ -18,54 +18,88 @@ function draw() {
   background(240);
   drawTarget();
 
-  // Вивід очок і часу  
+  // Вивід очок і часу (по центру зверху)
   fill(0);
+  textAlign(CENTER, CENTER);
+  textSize(24);
   text(`Очки: ${score}`, width / 2, 30);
   text(`Час: ${timeLeft}`, width / 2, 60);
 
-  if (!gameRunning && timeLeft === 0) {
-    fill(227, 38, 54);
-    textSize(32);
-     textAlign(RIGHT, TOP);
-    text("Час вийшов!", width , 0 );
-    textSize(24);
+  if (!gameRunning) {
+    if (timeLeft === 0) {
+      // Виведення повідомлення "Час вийшов!"
+      fill(227, 38, 54);
+      textSize(16);
+      textAlign(RIGHT, TOP);
+      text("Час вийшов!", width - 10, 10);
+
     
+      fill(0);
+      textAlign(CENTER, CENTER);
+      textSize(24);
+      text(`Фінальний рахунок: ${score}`, width / 2, height / 2 + 10);
+      textSize(24);
+      fill(100);
+      text("Натисніть ESCAPE, щоб оновити гру", width / 2, height / 2 + 50);
+
+    } else {
+      
+     
+      textAlign(LEFT, TOP); 
+      
+      let xOffset = 10; 
+      let yStart = 10;  
+      let lineSpacing = 30; 
+      
+      fill(0, 100, 0);
+      textSize(16);
+      text("ІНСТРУКЦІЇ:", xOffset, yStart);
+      
+     
+      textSize(12);
+      fill(0);
+      
+      text("Натисніть ENTER, щоб ПОЧАТИ", xOffset, yStart + lineSpacing * 2);
+      text("Натисніть ПРОБІЛ, щоб ЗУПИНИТИ/ПРОДОВЖИТИ", xOffset, yStart + lineSpacing * 3);
+      text("Натисніть ESCAPE, щоб ОНОВИТИ", xOffset, yStart + lineSpacing * 4);
+    }
   }
 }
 
 function drawTarget() {
   // Зовнішнє чорне коло
-  fill(0,0,0);
+  fill(0, 0, 0);
   ellipse(targetX, targetY, targetRadius * 4);
 
-  // Біле коло //fill('red');
-  fill(255,255,255);
+  // Біле коло
+  fill(255, 255, 255);
   ellipse(targetX, targetY, targetRadius * 3);
 
   // Внутрішнє чорне коло
-  fill(0,0,0);
+  fill(0, 0, 0);
   ellipse(targetX, targetY, targetRadius * 2);
-  
-  
-  // Біле коло //fill('red');
-  fill(255,255,255);
+
+  // Біле коло
+  fill(255, 255, 255);
   ellipse(targetX, targetY, targetRadius / 1);
 
   // Червоне центр
-  fill(255,0,0);
-  ellipse(targetX, targetY, targetRadius / 3);
+  fill(255, 0, 0);
+  ellipse(targetX, targetY, targetRadius / 5);
 }
-//Процес натискання
+
+// Процес натискання
 function mousePressed() {
   if (!gameRunning) return;
 
   let d = dist(mouseX, mouseY, targetX, targetY);
-  if (d < targetRadius / 2) {
+  if (d < targetRadius / 4) {
     score++;
     resetTarget();
   }
 }
-//Керування з клавіатури початок і оновити результат
+
+// Керування з клавіатури початок і оновити результат
 function keyPressed() {
   if (keyCode === ENTER) {
     startGame();
@@ -110,7 +144,8 @@ function resetGame() {
   timeLeft = 30;
   resetTarget();
 }
-//випадкове переміщення мішені
+
+// випадкове переміщення мішені
 function resetTarget() {
   targetX = random(targetRadius * 1.5, width - targetRadius * 1.5);
   targetY = random(targetRadius * 1.5 + 60, height - targetRadius * 1.5);
